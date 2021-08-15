ab83c12fd7c99616ebe249ff487565034720b634
Gleb Chebykin <chebykingi@gmail.com>
2021-08-15 12:29:11 +0300

    Initial project

---
+++ movies_admin/config/urls.py
@@ -0,0 +1,11 @@
+import debug_toolbar
+
+from django.contrib import admin
+from django.urls import include, path
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('movies.api.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+]
