5a41bfdb3654c3bbb9ef05f5654b7df1c181cc9b
Gleb Chebykin <chebykingi@gmail.com>
2021-08-21 13:29:17 +0300

    Use uuid as pk for Filmwork

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
 from model_utils.models import TimeStampedModel
 
+import uuid
 
 class Genre(TimeStampedModel):
     name = models.CharField(_('name'), max_length=255)
@@ -37,6 +38,7 @@ class FilmworkType(models.TextChoices):
 
 
 class Filmwork(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(_('title'), max_length=255)
     description = models.TextField(_('description'), blank=True, null=True)
     creation_date = models.DateField(_('creation date'), blank=True, null=True)
